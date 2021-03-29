63700917528514b6ae57a48529cb1803aecd37f1
Abdul <34360387+coolabudlg19@users.noreply.github.com>
2021-03-29 09:03:16 -0500

    Snake Game

---
+++ GamePanel.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+GamePanel(){
+
+    
+}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
